e53fcd89be5accef190c170fdeb60a0acb067e69
ebook178 <ebook178@gmail.com>
2025-04-12 15:13:04 +0800

    Remove EC2 to trigger destroy

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "myec2" {
-  ami = "ami-0100e595e1cc1ff7f"
-  instance_type = "t2.micro"
-  }
+#resource "aws_instance" "myec2" {
+#  ami = "ami-0100e595e1cc1ff7f"
+#  instance_type = "t2.micro"
+#  }
